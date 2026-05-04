a9c092254b284ed6d4e4530049cf08df9bb83a38
hhhgbhh04-tech <hhhgbhh04@gmail.com>
2026-05-04 19:42:05 +0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ddos_trastfactor",
-    version="2.0.0",
+    version="7.0.0",
     packages=find_packages(),
     install_requires=["aiohttp>=3.8.0"],
     python_requires=">=3.7",
