4e349d0d9af786f14322faa045447ed58e190393
hhhgbhh04-tech <hhhgbhh04@gmail.com>
2026-05-04 19:36:53 +0500

    Delete ddos_trastfactor directory

--- ddos_trastfactor/__init__.py
+++
@@ -1,7 +0,0 @@
-from .client import TrastClient
-from .tls import TLSBypass
-from .challenge import ChallengeBypass
-from .fingerprint import FingerprintSpoofer
-
-__version__ = "2.0.0"
-__all__ = ["TrastClient", "TLSBypass", "ChallengeBypass", "FingerprintSpoofer"]
